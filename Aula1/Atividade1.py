68332376b0c0ba49a3c57d8b8d41be37e65b9255
Loureni Monteiro Coutinho <65089431+Loureni-C@users.noreply.github.com>
2023-05-08 05:35:13 -0300

    Update Atividade1.py

--- Aula1/Atividade1.py
+++ Aula1/Atividade1.py
@@ -1,19 +1,37 @@
-aux = int(input('Digite um numero: '))
+def nove_r(n, d = 1):
+  s = 0
+  p = str(n)
+  l = len(p)
+  if l == 1:
+      return [n == 9, d]
+  else:
+    for c in p:
+      s += int(c)
+    return nove_r(s, d+1)
 
-a = 1
-if aux%9 == 0:
-   
-  while aux> 9:
-  
-    aux = sum(int (i) for i in str(aux))
-    if aux == 9:
-      a = a + 1
-      print("igual a 9")
-    else:
-      a = a + 1
-      aux = sum(int (i) for i in str(aux))
-  print("True")
- #print(aux)
-  print(a)
-else:
-  print("False")
+def nove_i(n):
+  d = 1
+  p = str(n)
+  l = len(p)
+  while l > 1:
+    s = 0
+    for c in p:
+      s += int(c)
+    n = s
+    d += 1
+    p = str(s)
+    l = len(p)
+  return [n == 9, d]
+
+r = nove_r(9)
+print(r)
+r = nove_r(99999999999999999999)
+print(r)
+r = nove_r(9999999999999999999999999999998)
+print(r)
+r = nove_i(9)
+print(r)
+r = nove_i(99999999999999999999)
+print(r)
+r = nove_i(9999999999999999999999999999998)
+print(r)
